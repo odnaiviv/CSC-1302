da7377b04ec6d3e64836ce61dc80693e3764975e
Vivian <odnaiviv@hotmail.com>
2021-08-25 14:41:19 -0400

    Update hw6_TicketClient.java

--- CSC1302_Homeworks/hw6/hw6_TicketClient.java
+++ CSC1302_Homeworks/hw6/hw6_TicketClient.java
@@ -1,16 +1,11 @@
 /*
- * Vivian Do
- * ID: 002-48-6640
  * July 20, 2021
  *
  * Homework 6: Task 1 (Ticket Client)
- * This is the Ticket Client class that is made to test out the Ticket, Walk-up Ticket, Advance Ticket, and Student Advance Ticket
- * classes. This class has the main method that will check to see if the results of these classes are correctly written and these results
- * are then printed out to show results.
  */
+
 public class hw6_TicketClient {
     public static void main(String[] args){
-        //creating new object from Ticket superclass
         //randomly picking numbers between 1 - 50 from an online random number generator
         //the days in advance are randomly generated number between 1 - 20
         hw6_Ticket t1 = new hw6_WalkupTicket(3);
