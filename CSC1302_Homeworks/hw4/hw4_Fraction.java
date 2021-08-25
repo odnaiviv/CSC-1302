2c7582ee86d6ecf2c2065f10c8e3cad94cfccf89
Vivian <odnaiviv@hotmail.com>
2021-08-25 14:02:39 -0400

    Update hw4_Fraction.java

--- CSC1302_Homeworks/hw4/hw4_Fraction.java
+++ CSC1302_Homeworks/hw4/hw4_Fraction.java
@@ -5,7 +5,7 @@
  */
 
 public class hw4_Fraction {
-    //instance variables = numerator and denominator of the fraction
+    //instance variables
     private int numerator;
     private int denominator;
 
@@ -32,25 +32,25 @@ public class hw4_Fraction {
         return new hw4_Fraction(num, denom);
     }
 
-    //constructor to set the numerator to 0 and the denominator to 1
+    //constructor to set numerator to 0 and denominator to 1
     public hw4_Fraction() {
         numerator = 0;
         denominator = 1;
     }
-    //constructor to initialize the numerator as num
+    //constructor to initialize numerator as num
     public hw4_Fraction(int num) {
         numerator = num;
         denominator = 1;
     }
 
-    //method to divide the fraction
+    //method to divide fraction
     public hw4_Fraction divide(hw4_Fraction f) {
         int num = numerator * f.denominator;
         int denom = denominator * f.numerator;
         return new hw4_Fraction(num, denom);
     }
 
-    //method to scale up the fraction
+    //method to scale up fraction
     public hw4_Fraction scaleup(int factor) {
         if(factor == 0) {
             System.out.println("ERROR! Fraction cannot be scaled by zero.");
@@ -61,7 +61,7 @@ public class hw4_Fraction {
         return new hw4_Fraction(numerator, denominator);
     }
 
-    //method to scale down the fraction
+    //method to scale down fraction
     public hw4_Fraction scaledown(int factor) {
         if(factor == 0) {
             System.out.println("ERROR! Fraction cannot be scaled by zero.");
@@ -72,7 +72,7 @@ public class hw4_Fraction {
         return new hw4_Fraction(numerator, denominator);
     }
 
-    //method to scale up or down the fraction based on the boolean
+    //method to scale up/down fraction based on boolean
     public hw4_Fraction scale(int factor, boolean flag) {
         if (flag) {
             return scaleup(factor);
