05947ed05ba6c74cf3aa54ec497d8ca536383371
Vivian <odnaiviv@hotmail.com>
2021-08-25 14:42:22 -0400

    Update hw6_WalkupTicket.java

--- CSC1302_Homeworks/hw6/hw6_WalkupTicket.java
+++ CSC1302_Homeworks/hw6/hw6_WalkupTicket.java
@@ -1,18 +1,14 @@
 /*
- * Vivian Do
- * ID: 002-48-6640
  * July 20, 2021
  *
  * Homework 6: Task 1 (Walk-up Ticket)
- * This is the Walk-up Ticket subclass that is constructed by a number with a price of $50.
  */
 public class hw6_WalkupTicket extends hw6_Ticket {
-    //initializing price as a double
     double price;
 
-    //constructor class to initialize variables
+    //constructor
     public hw6_WalkupTicket(int number) {
-        //super to call and refer to the ticket class object
+        //super to call & refer to the ticket class object
         super(number);
         //setting ticket price to $50
         price = 50.0;
