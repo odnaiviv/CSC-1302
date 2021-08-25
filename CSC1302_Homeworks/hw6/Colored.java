252009322875787ce394531b19b763e35775dc85
Vivian <odnaiviv@hotmail.com>
2021-08-25 14:30:14 -0400

    Update Colored.java

--- CSC1302_Homeworks/hw6/Colored.java
+++ CSC1302_Homeworks/hw6/Colored.java
@@ -1,13 +1,8 @@
 /*
- * Vivian Do
- * ID: 002-48-6640
  * July 20, 2021
  *
  * Homework 6: Task 2 (Colored)
- * This is the given Colored interface for this homework.
  */
-//ignore package because I'm using a different IDE
-package HW6;
 
 //Colored interface
 public interface Colored {
