b7560964a82da774ff8a7e9973945837e01210f9
Vivian <odnaiviv@hotmail.com>
2021-08-25 14:39:12 -0400

    Update hw6_StudentAdvanceTicket.java

--- CSC1302_Homeworks/hw6/hw6_StudentAdvanceTicket.java
+++ CSC1302_Homeworks/hw6/hw6_StudentAdvanceTicket.java
@@ -1,21 +1,16 @@
 /*
- * Vivian Do
- * ID: 002-48-6640
  * July 20, 2021
  *
  * Homework 6: Task 1 (Student Advance Ticket)
- * This is the Student Advance Ticket subclass that represents the purchased tickets in advance for students. This subclass
- * is constructed with a ticket number and the number of days the student has bought it in advance. The ticket price is $15 if the
- * student bought over 10 days. The ticket price is $20 if the student bought it in less than 10 days. The toString method will
- * ask student to show ID if ticket was bought in advance.
  */
+
 public class hw6_StudentAdvanceTicket extends hw6_Ticket{
-    //initializing private instance variable
+    //private instance variable
     private int stuDaysInAdvance;
 
-    //constructor to initialize variables
+    //constructor
     public hw6_StudentAdvanceTicket(int number, int stuDaysInAdvance) {
-        //super to call and refer to the ticket class object
+        //super to call & refer to the ticket class object
         super(number);
         //setting the number of days in advance for students as the current object
         this.stuDaysInAdvance = stuDaysInAdvance;
@@ -24,17 +19,17 @@ public class hw6_StudentAdvanceTicket extends hw6_Ticket{
     //first override method to return ticket price
     @Override
     public double getPrice() {
-        //if statement to check if student has bought ticket in more than 10 days in advance
+        //checking if student has bought ticket in more than 10 days in advance
         if (stuDaysInAdvance >= 10) {
             return 15;
         }
-        //else statement to check if student has nought ticket within 10 days
+        //checking if student has nought ticket within 10 days
         else {
             return 20;
         }
     }
 
-    //second override method to implement and return this toString method
+    //second override method to implement & return this toString method
     @Override
     public String toString() {
         return super.toString() + " (ID required)";
