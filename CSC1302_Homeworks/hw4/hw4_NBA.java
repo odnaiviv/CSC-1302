2baa761d65d633c152abd7a5c32d10cb3ca1ef3d
Vivian <odnaiviv@hotmail.com>
2021-08-25 14:10:05 -0400

    Update hw4_NBA.java

--- CSC1302_Homeworks/hw4/hw4_NBA.java
+++ CSC1302_Homeworks/hw4/hw4_NBA.java
@@ -1,17 +1,13 @@
 /*
- * Vivian Do
- * ID: 002-48-6640
  * July 06, 2021
  *
  * Homework 4: Task 2 (NBA)
- * This is the NBA class that will prompt the user to add players to each team.
- * This class also determines which team will win the overall simulated game series finished in the program.
  */
 import java.util.Scanner;
 public class hw4_NBA {
     public static void main(String[] args){
-        //initializing the given objects/variables
-        //changing the scanner from "input" to "scan"; this is my choice of preference
+        //initializing given objects/variables
+        //changing "input" to "scan"; this is my choice of preference
         Scanner scan = new Scanner(System.in);
         String ifAddPlayer;
         String playerName;
@@ -32,20 +28,18 @@ public class hw4_NBA {
 
         //constructing the Spurs Team
         //the code for creating the Spurs team is very similar to the code for the Heats team
-        //I will attempt to explain every line on how to set up the Spurs team
-        //prints the creation of the Spurs team
         System.out.println("Creating the NBA Team of Spurs ..... ");
         hw4_NBATeam Spurs = new hw4_NBATeam("Spurs");
         //prompts user to add players to the team or not
         System.out.println("Add a player to Spurs team? (yes/no): ");
         ifAddPlayer = scan.next();
-        //while loop to define and add player to team if user inputs yes
+        //defining and adding player to team if user inputs yes
         while(ifAddPlayer.equalsIgnoreCase("yes")) {
             System.out.print("What is the name of the player to be added? ");
             playerName = scan.next();
-            //stores the name into the array
+            //storing name into the array
             Spurs.addAPlayer(playerName);
-            //prompts user again to add another player onto the team
+            //prompts user again to add another player onto team
             System.out.print("Add another player to Spurs team? (yes/no): ");
             ifAddPlayer = scan.next();
             //loop will repeat until user inputs "no" to stop the loop
@@ -55,11 +49,11 @@ public class hw4_NBA {
         //the Heat wins if random number is bigger than 0.5; otherwise, the Heat loses
         //game is over as soon as one team wins 4 games
         System.out.println("Game on now... ");
-        //for loop to count the number of games in the series
+        //counting the number of games in the series
         for (int i = 0; i < 7; i++) {
-            //initializing double to generate a random number
+            //generating a random number
             double randomNum = Math.random();
-            //if else loop to determine which team is the winner of the game series
+            //determining which team is the winner of the game series
             if (randomNum > 0.5) {
                 Heat.winAGame();
                 Spurs.loseAGame();
@@ -69,7 +63,7 @@ public class hw4_NBA {
                 Spurs.winAGame();
             }
         }
-        //if else loop to determine which team wins the overall game series
+        //determining which team wins the overall game series
         if (Heat.winNum() > Spurs.winNum() || Spurs.winNum() > Heat.winNum()) {
             if (Heat.winNum() > Spurs.winNum()) {
                 System.out.println(Heat.teamName() + "***WIN the series***");
@@ -78,7 +72,7 @@ public class hw4_NBA {
                 System.out.println(Spurs.teamName() + "***WIN the series***");
             }
         }
-        //prints the score results of each team and the player on the team
+        //prints the score results of each team & player on the team
         System.out.println(Heat);
         System.out.println(Spurs);
     }
