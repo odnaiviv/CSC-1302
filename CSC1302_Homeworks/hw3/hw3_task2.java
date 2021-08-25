e099f2c28bbd822c99c1a0b40e43af78babd8803
Vivian <odnaiviv@hotmail.com>
2021-08-25 13:58:54 -0400

    Update hw3_task2.java

--- CSC1302_Homeworks/hw3/hw3_task2.java
+++ CSC1302_Homeworks/hw3/hw3_task2.java
@@ -1,11 +1,9 @@
 /*
- * Vivian Do
- * ID: 002-48-6640
  * June 29, 2021
  *
  * Homework 3: Task 2 (Drawing Patterns)
  */
-//importing all classes for creating user interfaces, painting graphics and images
+
 import java.awt.*;
 public class hw3_task2 {
     public static void main(String[] args){
@@ -34,14 +32,13 @@ public class hw3_task2 {
 
     //method to draw the circles and rectangles
     public static void CirclesAndRectangles (Graphics graphic, int x, int y, int circleNumber, int row, int column, int width, int height) {
-        //nested for loop draws the circles from the y direction and the x direction (in that order)
+        //drawing circles from y direction and then x direction
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= column; j++) {
-                //coloring the circles and rectangles with their respective colors
-                //draws and colors a green rectangle
+                //draws and colors rectangles green
                 graphic.setColor(Color.GREEN);
                 graphic.fillRect(x + (j - 1) * width, y + (i - 1) * height, width, height);
-                //draws and colors a yellow circle in front of the rectangle
+                //draws and colors circle yellow in front of rectangle
                 graphic.setColor(Color.YELLOW);
                 graphic.fillOval(x + (j - 1) * width, y + (i - 1) * height, width, height);
             }
@@ -49,21 +46,21 @@ public class hw3_task2 {
 
         //setting color back to black
         graphic.setColor(Color.BLACK);
-        //nest for loop for the number of rows and columns
+        //number of rows and columns
         for (int i = 1; i <= row; i++) {
             for (int j = 1; j <= column; j++) {
-                //draws the outside of rectangle to the close out the subfigure
+                //draws the outside of rectangle to close out the subfigure
                 graphic.drawRect(x + (j - 1) * width, y + (i - 1) * height, width, height);
-                //initializing the x and y axis for each subfigure
+                //an x & y for each subfigure
                 int xAxis = x + (j - 1) * width;
                 int yAxis = y + (i - 1) * height;
 
-                //calculating the spaces between each circle
+                //calculating spaces between each circle
                 int spaces = (int) Math.ceil(width / 10);
-                //decreases the space in the circle if the circle number is greater than 8
+                //decreases space in circle if circle number is greater than 8
                 if (circleNumber > 8)
                     spaces = spaces / 2;
-                //increases the space in the circle if the circle number is less than 5
+                //increases space in circle if circle number is less than 5
                 else if (circleNumber > 5)
                     spaces--;
 
@@ -72,11 +69,11 @@ public class hw3_task2 {
                     graphic.drawOval(xAxis + spaces * k, yAxis + spaces * k, width - spaces * 2 * k, height - spaces * 2 * k);
                 }
 
-                //draws the diagonal lines in each subfigure
+                //draws diagonal lines in each subfigure
                 graphic.drawLine(x + (j - 1) * width, y + (i - 1) * height, x + j * width, y + i * width);
                 graphic.drawLine(x + j * width, y + (i - 1) * height, x + (j - 1) * width, y + i * width);
 
-                //draws the horizontal and vertical lines across each subfigure
+                //draws horizontal and vertical lines across each subfigure
                 graphic.drawLine(x + (j - 1) * width + width / 2, y + (i - 1) * height, x + (j - 1) * width + width / 2, y + (i - 1) * height + height);
                 graphic.drawLine(x + (j - 1) * width, y + (i - 1) * height + height / 2, x + (j - 1) * width + width, y + (i - 1) * height + height / 2);
             }
