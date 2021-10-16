05dab588951afb10bdd7bd1e025f278e5e65c95f
Vivian <odnaiviv@hotmail.com>
2021-10-15 21:43:21 -0400

    Update lab3_task2.java

--- CSC1302_Labs/lab3/lab3_task2.java
+++ CSC1302_Labs/lab3/lab3_task2.java
@@ -1,15 +1,13 @@
 /*
- * Vivian Do
- * ID: 002-48-6640
  * June 16, 2021
  *
  * Lab 3: Task 2
  */
 //import user input scanner class
 import java.util.Scanner;
+
 public class lab3_task2 {
     public static void main(String[] args){
-        //creating user input scanner
         Scanner scan = new Scanner(System.in);
         //initializing temporary values for sum and count of inputted numbers
         double num = 0;
@@ -23,7 +21,7 @@ public class lab3_task2 {
         //while loop to check if inputted value is either positive or negative
         //if the input is negative, it will skip this loop
         while (num >= 0) {
-            //loop will read values
+            //reading values from loop
             count++;
             sum = sum + num;
 
