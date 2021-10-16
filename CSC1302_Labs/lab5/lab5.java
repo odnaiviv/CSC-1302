02c16bc9914562e4347755cd086b369d1a2ba288
Vivian <odnaiviv@hotmail.com>
2021-10-15 21:48:09 -0400

    Update lab5.java

--- CSC1302_Labs/lab5/lab5.java
+++ CSC1302_Labs/lab5/lab5.java
@@ -1,26 +1,24 @@
 /*
- * Vivian Do
- * ID: 002-48-6640
  * June 23, 2021
  *
  * Lab 5
  */
 //importing user input Scanner class
 import java.util.Scanner;
+
 public class lab5 {
     public static void main(String[] args){
-        //creating user input Scanner
         Scanner scan = new Scanner(System.in);
         //initializing elements as a variable
         int elements;
-        //prompting user to enter the amount of elements in an array
+        //prompting user to enter the number of elements in an array
         System.out.println("Enter an array of integers \nHow many elements: ");
         elements = scan.nextInt();
         //creating the array to store the elements in
         int array[] = new int[elements];
-        //prompting user to enter in the element components of the array
+        //prompting user to enter in the elements in the array
         System.out.println("What are the elements: ");
-        //for loop to count and input the elements in the array
+        //counting and inputting elements in the array
         for (int i = 0; i < array.length; i++) {
             array[i] = scan.nextInt();
         }
@@ -33,15 +31,15 @@ public class lab5 {
         //initializing temporary variables for the minimum and the difference
         int minimum = 0;
         int difference = 0;
-        //if loop to check the minimum if length of array is less than 2
+        //checking for the minimum if length of array is less than 2
         if (array.length < 2) {
             minimum = 0;
         }
-        //else loop to do the calculations between the number/elements
+        //calculating between the number/elements
         else {
             //subtracting the second element with the first element
             minimum = array[1] - array[0];
-            //for loop to calculate the difference
+            //calculating the difference
             for (int i = 2; i < array.length - 1; i++) {
                 difference = array[i + 1] - array[i];
                 if (difference < minimum) {
