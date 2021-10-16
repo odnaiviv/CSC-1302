af0ae68541f46f8acdbec7f5ddafdd4300f92c99
Vivian <odnaiviv@hotmail.com>
2021-10-15 21:42:35 -0400

    Update lab3_task1.java

--- CSC1302_Labs/lab3/lab3_task1.java
+++ CSC1302_Labs/lab3/lab3_task1.java
@@ -1,6 +1,4 @@
 /*
- * Vivian Do
- * ID: 002-48-6640
  * June 16, 2021
  *
  * Lab 3: Task 1
@@ -9,16 +7,16 @@ public class lab3_task1 {
     public static void main(String[] args){
         //for loop to start counting rows down with 5
         for (int count = 5; count >= 1; count--) {
-            //for loop to print numbers
+            //printing numbers
             for (int num = count; num <= 5; num++) {
-                //prints and spaces out numbers
+                //printing spaced-out numbers
                 System.out.print(num + " ");
             }
-            //if else loop to stop spacing between rows
+            //stopping the spacing between rows
             if (count != 2) {
                 System.out.println();
             }
-            //returns to next line and continues printing out rest of the program
+            //returns to next line & program continues
             else{
                 System.out.println();
                 continue;
